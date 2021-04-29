634a50fed23cb8e4a3bbece76d7e18a57f2ba725
Hang Kim duyen <duyenhang@poetadigital.com>
2021-04-29 15:07:41 +0700

    home screen

--- HomeScreen/styles.js
+++
@@ -1,6 +0,0 @@
-import { StyleSheet } from 'react-native'
-import { colors, spaces } from 'themes'
-
-export default StyleSheet.create({
- 
-});
--- src/scenes/HomeScreen.js
+++ src/scenes/HomeScreen.js
@@ -1,14 +1,51 @@
 import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import {
+    StyleSheet, Button, View, SafeAreaView, Text, Alert,
+    FlatList,
+    TouchableOpacity
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Video from 'react-native-video';
+import { WebView } from 'react-native-webview';
 
 const Separator = () => (
     <View style={styles.separator} />
 );
 
+const onPressItem = (item) => { }
+
+const renderItem = (item) => {
+    return (
+      <View>
+        <View
+            style={styles.containerItem}>
+            
+            <WebView
+                source={{ uri: 'https://chunk.lab.zalo.ai/9abcbb353264db3a8275/9abcbb353264db3a8275' }}
+                style={styles.videoStyle}
+            />
+            <MediaControls
+                        mainColor="#333"
+                        showOnStart={false}
+                    ></MediaControls>
+            <View >
+                <Text style={styles.text}>{item.title}</Text>
+                <Text style={styles.textContent}>{item.content}</Text>
+            </View>
+            </View>
+            <View style={ styles.separateLine}/>
+    </View>
+  )
+}
+const listData = [{title:'test', content: 'abc', url: ''}]
+
 const HomeScreen = ({ navigation }) => (
     <SafeAreaView style={styles.container}>
+         <FlatList
+            data={listData}
+            renderItem={({ item }) => renderItem(item)}
+            keyExtractor={(item, index) => index.toString()} />
         <Button
             onPress={() => { navigation.navigate('AddNewStory') }}
             title="Add New"
@@ -24,19 +61,34 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginHorizontal: 16,
     },
-    title: {
-        textAlign: 'center',
-        marginVertical: 8,
-    },
-    fixToText: {
+    containerItem: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'flex-start',
+        padding: 20
     },
-    separator: {
-        marginVertical: 8,
-        borderBottomColor: '#737373',
-        borderBottomWidth: StyleSheet.hairlineWidth,
+    text: {
+        paddingLeft: 25,
+        paddingRight: 25,
+        color: "#0000ff",
+        fontSize: 20,
+        fontWeight: '500',
     },
+    textContent: {
+        paddingLeft: 25,
+        paddingRight: 25,
+        color: "#595959",
+        fontSize: 12,
+        fontWeight: '500',
+    },
+    videoStyle: {
+        backgroundColor: '#0000ff',
+        height: 50,
+        width: 50,
+    },
+    separateLine: {
+        height: 1,
+        backgroundColor: "#999999",
+    }
 });
 
 export default HomeScreen;
